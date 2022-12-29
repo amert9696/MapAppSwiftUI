169d53a6247672d5cfc62ee7dc104dd679f3c488
AMG <alimertgulec38@gmail.com>
2022-12-29 22:24:55 +0300

    Initial Commit

---
+++ MapAppSwiftUI/MapAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppSwiftUIApp.swift
+//  MapAppSwiftUI
+//
+//  Created by Ali Mert Güleç on 29.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MapAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
